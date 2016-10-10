ff1c5855834464ad82597f32b8e3d11845376686
Lee Broxson <broxsonl@gmail.com>
2016-10-10 11:51:36 -0700

    Required in necessary modules to auth-router-test.

--- test/auth-router-test.js
+++ test/auth-router-test.js
@@ -1,1 +1,21 @@
 'use strict';
+
+require('./lib/test-env.js');
+const expect = require('chai').expect;
+const request = require('superagent');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const serverCtrl = require('./lib/server-ctrl.js');
+const cleanDB = require('./lib/clean-db.js');
+const mockUser = require('./lib/user-mock.js');
+
+mongoose.Promise = Promise;
+
+const server = require('../server.js');
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'slugbyte',
+  password: '12345678',
+  email: 'slug@slug.slime',
+};
