0d57f541b3b134a420b95d4f95cdd7e514c00a96
meeksfred <maxfred0025@gmail.com>
2016-10-10 15:10:25 -0700

    remove photo property from listing, begin listing mock

---
+++ test/lib/listing-mock.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const debug = require('debug');
+const galleryMock = require('./gallery-mock.js');
+const Listing = require('../../model/listing.js');
+
+module.exports = function(done){
+  debug('create mock listing');
+  let exampleListing = {
+    
+  }
+}
