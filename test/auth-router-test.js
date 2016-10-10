f3fde5de67388957b266984fc0ded821669403bc
Lee Broxson <broxsonl@gmail.com>
2016-10-10 12:01:10 -0700

    Wrote test for a good POST request.

--- test/auth-router-test.js
+++ test/auth-router-test.js
@@ -1,21 +1,57 @@
 'use strict';
 
+// bringing in test environment
 require('./lib/test-env.js');
+
+// npm modules
 const expect = require('chai').expect;
 const request = require('superagent');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
-const serverCtrl = require('./lib/server-ctrl.js');
+
+// app modules
+const serverCtrl = require('./lib/server-control.js');
 const cleanDB = require('./lib/clean-db.js');
-const mockUser = require('./lib/user-mock.js');
+const mockUser = require('./lib/user-mocks.js');
 
 mongoose.Promise = Promise;
 
+// module constants
 const server = require('../server.js');
 const url = `http://localhost:${process.env.PORT}`;
 
 const exampleUser = {
-  username: 'slugbyte',
-  password: '12345678',
-  email: 'slug@slug.slime',
+  username: 'fakeuser',
+  password: '192837465',
+  email: 'fake@fakestuff.fake',
 };
+
+describe('testing auth-router', function() {
+
+  before( done => serverCtrl.serverUp(server, done));
+
+  after( done => serverCtrl.serverDown(server, done));
+
+  afterEach( done => cleanDB(done));
+
+  describe('testing POST /api/signup', function() {
+
+    describe('with a valid body', function() {
+
+      it('should return a token', (done) => {
+
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(!!res.text).to.equal(true);
+          done();
+        });
+      });
+    });
+    //TODO: More tests for POST here.
+
+  });
+});
