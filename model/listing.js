5703c9d849ca4b67e87b10e924b40e5e2eee824c
Claudia <claudia.cedfeldt@gmail.com>
2016-10-10 14:20:45 -0700

    edited models

--- model/listing.js
+++ model/listing.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose');
 const listingSchema = mongoose.Schema({
   title: {type: String, required: true},
   desc: {type: String, required: true},
-  category: {type: String, required: true},
+  category: {type: String, required: true}, // validated string
   photo: {type: String, required: true},
   username: {type: String, required: true},
   created: {type: Date, required: true, default: Date.now},
   galleryID: {type: mongoose.Schema.Types.ObjectId, required: true},
-  profileID: {type: mongoose.Schema.Types.ObjectId, required: true},
+  artistID: {type: mongoose.Schema.Types.ObjectId, required: true},
+  photoID: {type: mongoose.Schema.Types.ObjectId},
+
 });
 
 module.exports = mongoose.model('listing', listingSchema);
