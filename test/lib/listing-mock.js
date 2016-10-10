c0ca2ebf29baa59912324d8947463bd228cc6957
meeksfred <maxfred0025@gmail.com>
2016-10-10 15:16:00 -0700

    added listing mock

--- test/lib/listing-mock.js
+++ test/lib/listing-mock.js
@@ -7,6 +7,20 @@ const Listing = require('../../model/listing.js');
 module.exports = function(done){
   debug('create mock listing');
   let exampleListing = {
-    
-  }
-}
+    title: 'a cat',
+    desc: 'george does cat portraits',
+    category: 'portraits',
+  };
+  galleryMock.call(this, err => {
+    if (err)
+      return done(err);
+    exampleListing.username = this.tempUser.username;
+    exampleListing.artistID = this.tempArtist._id.toString();
+    exampleListing.galleryID = this.tempGallery._id.toString();
+    new Listing(exampleListing).save()
+    .then( listing => {
+      this.tempListing = listing;
+      done();
+    });
+  });
+};
