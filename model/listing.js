4488917028f820279b98588b2cb9d8fdfdb54eb7
Claudia <claudia.cedfeldt@gmail.com>
2016-10-10 12:02:45 -0700

    finished artist listing and photo models

--- model/listing.js
+++ model/listing.js
@@ -3,16 +3,14 @@
 const mongoose = require('mongoose');
 
 const listingSchema = mongoose.Schema({
-  //title
-  //description
-  //Category
-  //photo[pop]
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  category: {type: String, required: true},
+  photo: {type: String, required: true},
+  username: {type: String, required: true},
   created: {type: Date, required: true, default: Date.now},
-  //username
   galleryID: {type: mongoose.Schema.Types.ObjectId, required: true},
   profileID: {type: mongoose.Schema.Types.ObjectId, required: true},
-
-
 });
 
 module.exports = mongoose.model('listing', listingSchema);
