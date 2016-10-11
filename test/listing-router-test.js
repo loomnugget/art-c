a449a3d8fc351783a157850f19c9d5875e0a4967
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-10-11 12:08:10 -0700

    added router, fixing test and router

--- test/listing-router-test.js
+++ test/listing-router-test.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 // app modules
 const serverCtrl = require('./lib/server-control.js');
 const cleanDB = require('./lib/clean-db.js');
-// const mockGallery = require('./lib/gallery-mock.js');
+const mockGallery = require('./lib/gallery-mock.js');
 // const mockListing = require('./lib/listing-mock.js');
 
 mongoose.Promise = Promise;
@@ -37,18 +37,26 @@ describe('testing listing-router', function(){
   describe('testing POST /api/signup', function(){
 
     describe('with a valid body', function(){
-      it('should return a token', done => {
-        request.post(`${url}/api/artist/${this.tempArtist._id}/listing`)
-        .set({Authorization: `Bearer ${this.tempToken}`})
+
+      before(done => mockGallery.call(this, done));
+
+      it('should return a listing and status 200', done => {
+        request.post(`${url}/api/gallery/${this.tempGallery._id}/listing`)
         .send(exampleListing)
+        .set({Authorization: `Bearer ${this.tempToken}`})
         .end((err, res) => {
+          console.log(res.body);
           if(err) return done(err);
           expect(res.status).to.equal(200);
           expect(res.body.title).to.equal(exampleListing.title);
           expect(res.body.desc).to.equal(exampleListing.desc);
           expect(res.body.category).to.equal(exampleListing.category);
-          expect(res.body.artistID).to.equal(this.tempArtist._id);
-          expect(res.body.galleryID).to.equal(this.tempGallery);
+          expect(res.body.username).to.equal(this.tempArtist.username);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
           done();
         });
       });
