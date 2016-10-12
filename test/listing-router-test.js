6207c1aca041b0157f7f131c048ac66399571f59
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-10-11 20:42:39 -0700

    added the PUT and DELETE tests, 93 passing

--- test/listing-router-test.js
+++ test/listing-router-test.js
@@ -12,7 +12,6 @@ const mongoose = require('mongoose');
 // app modules
 const serverCtrl = require('./lib/server-control.js');
 const cleanDB = require('./lib/clean-db.js');
-const mockUser = require('./lib/gallery-mock.js');
 const mockGallery = require('./lib/gallery-mock.js');
 const mockListing = require('./lib/listing-mock.js');
 
@@ -298,6 +297,329 @@ describe('testing listing-router', function(){
       });
     });
 
+  });
+
+  describe('testing PUT to /api/listing/:listingID', () => {
+
+    describe('updating title property with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {title: 'bob'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(updateData.title);
+          expect(res.body.desc).to.equal(this.tempListing.desc);
+          expect(res.body.category).to.equal(this.tempListing.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating desc property with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {desc: 'bob'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(this.tempListing.title);
+          expect(res.body.desc).to.equal(updateData.desc);
+          expect(res.body.category).to.equal(this.tempListing.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating category property with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {category: 'bob'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(this.tempListing.title);
+          expect(res.body.desc).to.equal(this.tempListing.desc);
+          expect(res.body.category).to.equal(updateData.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating title and category properties with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {title: 'bob', category: 'bob2'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(updateData.title);
+          expect(res.body.desc).to.equal(this.tempListing.desc);
+          expect(res.body.category).to.equal(updateData.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating title and desc properties with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {title: 'bob', desc: 'bob2'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(updateData.title);
+          expect(res.body.desc).to.equal(updateData.desc);
+          expect(res.body.category).to.equal(this.tempListing.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating desc and category properties with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {desc: 'bob', category: 'bob2'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempListing.username);
+          expect(res.body.title).to.equal(this.tempListing.title);
+          expect(res.body.desc).to.equal(updateData.desc);
+          expect(res.body.category).to.equal(updateData.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('updating title, desc and category properties with valid token and id, ', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should return a listing', done => {
+        let updateData = {title: 'bob', desc: 'bob2', category: 'bob3'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.username).to.equal(this.tempGallery.username);
+          expect(res.body.title).to.equal(updateData.title);
+          expect(res.body.desc).to.equal(updateData.desc);
+          expect(res.body.category).to.equal(updateData.category);
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          expect(res.body.artistID).to.equal(this.tempArtist._id.toString());
+          expect(res.body.galleryID).to.equal(this.tempGallery._id.toString());
+          let date = new Date(res.body.created).toString();
+          expect(date).to.not.equal('Invalid Date');
+          done();
+        });
+      });
+    });
+
+    describe('with valid token and invalid id', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should status 404 not found', done => {
+        let updateData = {name: 'bob'};
+        request.put(`${url}/api/listing/${this.tempListing._id}bad`)
+        .send(updateData)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+
+    describe('with invalid token and valid id', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should status 401 unauthorized', done => {
+        let updateData = {title: 'bob'};
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .send(updateData)
+        .set({
+          Authorization: 'Bearer ',
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    });
+
+    describe('with wrong user', function(){
+
+      before(done => mockListing.call(this, done));
+      // before(done => mockUser.call(this, done));
+
+      it('should status 401 unauthorized', done => {
+        request.put(`${url}/api/listing/${this.tempListing._id}`)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('testing DELETE to /api/listing/:listingID', () => {
+
+    describe('with valid token and id', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should delete a listing', done => {
+        request.delete(`${url}/api/listing/${this.tempListing._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err)
+            return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+      });
+    });
+
+    describe('with valid token and invalid id', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should status 404 not found', done => {
+        request.delete(`${url}/api/listing/${this.tempListing._id}bad`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+
+    describe('with invalid token and valid id', function(){
+
+      before(done => mockListing.call(this, done));
+
+      it('should status 401 unauthorized', done => {
+        request.delete(`${url}/api/listing/${this.tempListing._id}`)
+        .set({
+          Authorization: 'Bearer ',
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    });
+
+    describe('with wrong user', function(){
+
+      before(done => mockListing.call(this, done));
+      // before(done => mockUser.call(this, done));
+
+      it('should status 401 unauthorized', done => {
+        request.delete(`${url}/api/listing/${this.tempListing._id}`)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          done();
+        });
+      });
+    });
   });
 
 });
