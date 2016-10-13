bc07f7b0d00969f47d0573625dc1599b0fedf528
meeksfred <maxfred0025@gmail.com>
2016-10-13 15:41:16 -0700

    added complete delete functionality for artist route

--- route/artist-router.js
+++ route/artist-router.js
@@ -5,13 +5,20 @@ const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const debug = require('debug')('artc:artist-route');
 const createError = require('http-errors');
+const AWS = require('aws-sdk');
 
 // app modules
 const Artist = require('../model/artist.js');
+const Gallery = require('../model/gallery.js');
+const Listing = require('../model/listing.js');
+const Photo = require('../model/photo.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
+
+AWS.config.setPromisesDependency(require('bluebird'));
 
 // module constants
 const artistRouter = module.exports = Router();
+const s3 = new AWS.S3();
 
 artistRouter.post('/api/artist', bearerAuth, jsonParser, function(req, res, next) {
   debug('POST /api/artist');
@@ -40,7 +47,7 @@ artistRouter.put('/api/artist/:artistID', bearerAuth, jsonParser, function(req, 
   .catch(err => Promise.reject(createError(404, err.message)))
   .then( artist => {
     if (artist.userID.toString() !== req.user._id.toString())
-      return next(createError(401, 'invalid userid'));
+      return Promise.reject(createError(401, 'invalid userid'));
     return Artist.findByIdAndUpdate(req.params.artistID, req.body, {new: true, runValidators: true});
   })
   .then(artist => res.json(artist))
@@ -53,9 +60,24 @@ artistRouter.delete('/api/artist/:artistID', bearerAuth, function(req, res, next
   .catch(err => Promise.reject(createError(404, err.message)))
   .then( artist => {
     if (artist.userID.toString() !== req.user._id.toString())
-      return next(createError(401, 'unauthorized'));
+      return Promise.reject(createError(401, 'unauthorized'));
     return Artist.findByIdAndRemove(req.params.artistID);
   })
+  .catch( err => Promise.reject(err, err.message))
+  .then( () => Listing.remove({ artistID: req.params.artistID}))
+  .then( () => Gallery.remove({ artistID: req.params.artistID}))
+  .then( () => Photo.find({ artistID: req.params.artistID}))
+  .then( photos => {
+    let s3DeletePhotoArray = [];
+    for(var i=0; i<photos.length; i++){
+      s3DeletePhotoArray.push(s3.deleteObject({
+        Bucket: 'artc-staging-assets',
+        Key: photos[i].objectKey,
+      }).promise());
+    }
+    return Promise.all(s3DeletePhotoArray);
+  })
+  .then( () => Photo.remove({ artistID: req.params.artistID}))
   .then( () => res.sendStatus(204))
   .catch(next);
 });
