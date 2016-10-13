e95037e0eecb5fc4fa2ec48d0cff620ee402aac2
Claudia <claudia.cedfeldt@gmail.com>
2016-10-13 14:46:28 -0700

    ready for the first test- pagenation only

--- route/page-router.js
+++ route/page-router.js
@@ -2,13 +2,13 @@
 
 // npm modules
 const Router = require('express').Router;
-const jsonParser = require('body-parser').json();
+//const jsonParser = require('body-parser').json();
 const debug = require('debug')('artc:gallery-route');
-const createError = require('http-errors');
+//const createError = require('http-errors');
 const AWS = require('aws-sdk');
 
 // app modules
-//const Artist = require('../model/artist.js');
+const Artist = require('../model/artist.js');
 const Gallery = require('../model/gallery.js');
 //const Photo = require('../model/photo.js');
 //const Listing = require('../model/listing.js');
@@ -20,27 +20,22 @@ AWS.config.setPromisesDependency(require('bluebird'));
 // module constants
 const pageRouter = module.exports = Router();
 
-pageRouter.get('/api/gallery', pageMiddleware, function(req, res, next){
-  debug('hit route GET /api/fruit');
-  let offset = req.query.offset;
-  let pagesize = req.query.pagesize;
-  let page = req.query.page;
-  let skip = offset + pagesize * page;
+pageRouter.get('/api/artist', pageMiddleware, function(req, res, next){
+  debug('hit route GET /api/artist');
+  let offset = req.query.offset, pageSize = req.query.pagesize, page = req.query.page;
 
-  Gallery.find().skip(skip).limit(pagesize) 
-  .then(galleries => res.json(galleries))
+  let skip = offset + pageSize * page ;
+  Artist.find().skip(skip).limit(pageSize)
+  .then( artists => res.json(artists))
   .catch(next);
 });
 
-pageRouter.get('/api/listing', pageMiddleware, function(req, res, next){
-  debug('hit route GET /api/listing');
-  let offset = req.query.offset;
-  let pagesize = req.query.pagesize;
-  let page = req.query.page;
-  let skip = offset + pagesize * page;
+pageRouter.get('/api/gallery', pageMiddleware, function(req, res, next){
+  debug('hit route GET /api/gallery');
+  let offset = req.query.offset, pageSize = req.query.pagesize, page = req.query.page;
 
-  Listing.find().skip(skip).limit(pagesize)
-
-  .then(listings => res.json(listings))
+  let skip = offset + pageSize * page ;
+  Gallery.find().skip(skip).limit(pageSize)
+  .then( galleries => res.json(galleries))
   .catch(next);
 });
--- test/page-router-test.js
+++ test/page-router-test.js
@@ -19,13 +19,23 @@ const cleanDB = require('./lib/clean-db');
 //const mockManyGalleries = require('./lib/populate-artist-galleries-mock.js');
 const mockManyListings = require('./lib/populate-gallery-listings-mock.js');
 
-
 mongoose.Promise = Promise;
 
 // app constants
 const server = require('../server.js');
 const url = `http://localhost:${process.env.PORT}`;
 
+const exampleGallery = {
+  name: 'Happy Stuff',
+  desc: 'this is the best album',
+  category: 'fun',
+};
+
+//TESTS NEEDED
+// artist has pages of galleries
+// galleries have pages of listings
+// listings have pages of photos
+// should
 
 describe('testing page-router', function(){
   //start/stop server and clean database
@@ -35,32 +45,72 @@ describe('testing page-router', function(){
 
  // test for page queries for many listings in a gallery with a given ID
   describe('testing /api/gallery', function() {
-    describe('with ?itemcount=10&itempage=2',  function() {
-      // mock 100 listings
-      before(done => mockManyListings.call(this, 100, done));
-      it('should return a gallery', done => {
-        request.get(`${url}/api/gallery/${this.tempGallery._id}?itemcount=10&itempage=2`)
-        .set({
-          Authorization: `Bearer ${this.tempToken}`,
-        })
+    describe('with pagenation' , function() {
+      it('should return 50 galleriyes', done => {
+        request.get(`${url}/api/gallery`)
         .end((err, res) => {
           if (err) return done(err);
-          expect(res.body.name).to.equal(exampleGallery.name);
-          expect(res.body.desc).to.equal(exampleGallery.desc);
-          expect(res.body.userID).to.equal(this.tempUser._id.toString());
-          expect(Array.isArray(res.body.pics)).to.equal(true);
-          expect(res.body.pics.length).to.equal(10);
-          let date = new Date(res.body.created).toString();
-          expect(date).to.equal(this.tempGallery.created.toString());
-          for (let i=0; i< res.body.pics.length; i++){
-            expect(res.body.pics[i]._id.toString()).to.equal(this.tempPics[i + 10 ]._id.toString());
-            expect(res.body.pics[i].name).to.equal(this.tempPics[i + 10].name);
-            expect(res.body.pics[i].desc).to.equal(this.tempPics[i + 10].desc);
-            expect(res.body.pics[i].imageURI).to.equal(this.tempPics[i + 10].imageURI);
-          }
-          done()
-        })
-      })
-    })
-  });
-});
+          expect(res.status).to.equal(200);
+          expect(res.body.length).to.equal(50);
+          done();
+        });
+      });
+    });
+
+
+    // describe('with ?page=2',  function() {
+    //   // mock 100 listings
+    //   before(done => mockManyListings.call(this, 100, done));
+    //   it('should return a page of listings', done => {
+    //     request.get(`${url}/api/gallery/${this.tempGallery._id}?page=2`)
+    //     .set({ Authorization: `Bearer ${this.tempToken}`})
+    //     .end((err, res) => {
+    //       if (err) return done(err);
+    //       expect(res.body.name).to.equal(exampleGallery.name);
+    //       expect(res.body.desc).to.equal(exampleGallery.desc);
+    //       expect(res.body.userID).to.equal(this.tempUser._id.toString());
+    //       expect(Array.isArray(res.body.listings)).to.equal(true);
+    //       expect(res.body.pics.length).to.equal(10);
+    //       let date = new Date(res.body.created).toString();
+    //       expect(date).to.equal(this.tempGallery.created.toString());
+    //       // iterate though listings array on gallery
+    //       for (let i=0; i< res.body.listings.length; i++){
+    //         //check if listings id  from the database is the same as query
+    //         expect(res.body.listings[i]._id.toString()).to.equal(this.tempListings[i + 10 ]._id.toString());
+    //         expect(res.body.listings[i].name).to.equal(this.tempListings[i + 10].name);
+    //         expect(res.body.listings[i].desc).to.equal(this.tempListings[i + 10].desc);
+    //       }
+    //       done();
+    //     });
+    //   }); // end it block
+    // });
+    //
+    // describe('with many listings', function(){
+    //   // mock 100 listings
+    //   before(done => mockManyListings.call(this, 100, done));
+    //   it('should return a gallery', done => {
+    //     request.get(`${url}/api/gallery/${this.tempGallery._id}`)
+    //     .set({Authorization: `Bearer ${this.tempToken}`})
+    //    .end((err, res) => {
+    //      if (err) return done(err);
+    //      expect(res.body.name).to.equal(exampleGallery.name);
+    //      expect(res.body.desc).to.equal(exampleGallery.desc);
+    //      expect(res.body.userID).to.equal(this.tempUser._id.toString());
+    //      expect(Array.isArray(res.body.listings)).to.equal(true);
+    //      expect(res.body.pics.length).to.equal(10);
+    //      let date = new Date(res.body.created).toString();
+    //      expect(date).to.equal(this.tempGallery.created.toString());
+    //      // iterate though listings array on gallery
+    //      for (let i=0; i< res.body.listings.length; i++){
+    //        //check if listings id  from the database is the same as query
+    //        expect(res.body.listings[i]._id.toString()).to.equal(this.tempListings[i + 10 ]._id.toString());
+    //        expect(res.body.listings[i].name).to.equal(this.tempListings[i + 10].name);
+    //        expect(res.body.listings[i].desc).to.equal(this.tempListings[i + 10].desc);
+    //      }
+    //      done();
+    //    });
+    //   }); // end it block
+    // });
+
+  }); // end testing /api/gallery
+}); // end first describe block
