620c704443333d7604969fe35715acbd776ed0fd
Claudia <claudia.cedfeldt@gmail.com>
2016-11-11 11:12:19 -0800

    fixed package.json

--- app/entry.js
+++ app/entry.js
@@ -21,35 +21,13 @@ const ngFileUpload = require('ng-file-upload');
 
 // Create angular module
 // Add everything we need to module
-const demoApp = angular.module(camelcase(__TITLE__), [ngTouch, ngAnimate, uiRouter, ngFileUpload]);
-
-// create angular module
-// set up $rootScope globals
-demoApp.run(['$rootScope', function($rootScope){
-  $rootScope.title = __TITLE__;
-}]);
-
-// WHAT DOES REQUIRE.CONTEXT DO?
-//////////////////////////////////////
-// 1. Loops through directory
-// 2. Gives us path
-// 3. Gives us the object to export
-
-// BASENAME = name of file without folders or .js
-// ex: start with app/goose/file.js --> returns 'file'
-// true parameter -looks through sub-directories
-// Require.context returns a function
-
-// Returns object that has files that match regex in the config directory and loads it in
-// Key - absolute path of where file was found
-// Context has a method called keys that returns an array of paths
-// For each loops across it and gives us the path name
+const app = angular.module('app', [ngTouch, ngAnimate, uiRouter, ngFileUpload]);
 
 // LOAD CONFIG
 let context = require.context('./config/', true, /.js$/);
 // Goes through config and finds files that end .js
 context.keys().forEach( path => { // Gets array of paths -  path is the name returned
-  demoApp.config(context(path)); // Gives us the object (module that is exported)
+  app.config(context(path)); // Gives us the object (module that is exported)
 });
 
 // LOAD VIEW CONTROLLERS
@@ -62,7 +40,7 @@ context.keys().forEach( key => {
   // Name controller based on file name
   // Pass in path into context -returns what was exported
   let module = context(key); // value of module.exports
-  demoApp.controller(name, module);
+  app.controller(name, module);
 });
 
 // Load services
@@ -70,7 +48,7 @@ context = require.context('./service/', true, /.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  demoApp.service(name, module);
+  app.service(name, module);
 });
 
 // Load components
@@ -78,7 +56,7 @@ context = require.context('./component/', true, /.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  demoApp.component(name, module);
+  app.component(name, module);
 });
 
 // Load filters
@@ -86,5 +64,5 @@ context = require.context('./filter/', true, /.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  demoApp.filter(name, module);
+  app.filter(name, module);
 });
