67ce99689f9705a34e266cb6297b66533b0d2c22
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-11-14 15:03:30 -0800

    workingworking on my first test

--- client-test/auth-service-test.js
+++ client-test/auth-service-test.js
@@ -1,1 +1,32 @@
 'use strict';
+
+describe('testing authservice', function() {
+
+  beforeEach(() => {
+    angular.mock.module('app');
+    angular.mock.inject((authService, $q, $window, $rootScope, $httpBackend) => {
+
+      this.authService = authService;
+      this.$q = $q;
+      this.$window = $window;
+      this.$rootScope = $rootScope;
+      this.$httpBackend = $httpBackend;
+    });
+  });
+
+  describe('#getToken() ', () => {
+    it('should return token', () => {
+
+      this.authService.token = null;
+      this.$window.localStorage.setItem('token', 'Dibbles the Tribble');
+
+      this.authService.getToken()
+      .then(token => {
+        expect(token).toEqual('Dibbles the Wibble');
+      });
+
+      this.$rootScope.$apply();
+    });
+  });
+
+});
