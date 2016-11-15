2355b52692fd81150602364dfafdd02a13bd4567
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-11-14 16:22:03 -0800

    got getToken() and setToken() working, now

--- client-test/auth-service-test.js
+++ client-test/auth-service-test.js
@@ -4,25 +4,42 @@ describe('testing authservice', function() {
 
   beforeEach(() => {
     angular.mock.module('app');
-    angular.mock.inject((authService, $q, $window, $rootScope, $httpBackend) => {
+    angular.mock.inject((authService, $window, $rootScope, $httpBackend) => {
 
       this.authService = authService;
-      this.$q = $q;
+      authService.setToken('Higgity Piggity');
+
       this.$window = $window;
       this.$rootScope = $rootScope;
       this.$httpBackend = $httpBackend;
     });
   });
 
-  describe('#getToken() ', () => {
+  afterEach(() => {
+    this.authService.token = null;
+    this.$window.localStorage.clear();
+  });
+
+  describe('#getToken()', () => {
     it('should return token', () => {
 
-      this.authService.token = null;
-      this.$window.localStorage.setItem('token', 'Dibbles the Tribble');
+      this.authService.token = 'Dibble the Wibble';
 
       this.authService.getToken()
       .then(token => {
-        expect(token).toEqual('Dibbles the Wibble');
+        expect(token).toEqual('Dibble the Wibble');
+      });
+
+      this.$rootScope.$apply();
+    });
+  });
+
+  describe('#setToken()', () => {
+    it('should set a token', () => {
+
+      this.authService.setToken('Fibbity Bibbity')
+      .then(token => {
+        expect(token).toEqual('Fibbity Bibbity');
       });
 
       this.$rootScope.$apply();
