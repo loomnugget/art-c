e732b3edb9cd4b48fa5e541fdfe1dacade0e4b21
Claudia <claudia.cedfeldt@gmail.com>
2016-11-15 12:00:46 -0800

    got carousel working

--- app/component/landing/carousel/carousel.js
+++ app/component/landing/carousel/carousel.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('./_carousel.scss');
+
 module.exports = {
   template: require('./carousel.html'),
   controller: ['$log', CarouselController],
@@ -8,19 +10,21 @@ module.exports = {
 
 function CarouselController($log){
   $log.debug('init carouselCtrl');
+
+  this.active = 0;
   this.interval = 3000;
   this.slides = [
     {
       image: 'http://i68.tinypic.com/2wbt6if.jpg',
+      id: 0,
     },
     {
       image: 'http://i67.tinypic.com/20qxv9l.jpg',
+      id: 1,
     },
     {
       image: 'http://i66.tinypic.com/oqetlk.jpg',
-    },
-    {
-      image: 'http://i66.tinypic.com/oqetlk.jpg',
+      id:2,
     },
   ];
 }
