35c5c515fe9b3ec3334f92943be1f51a68cb87cc
meeksfred <maxfred0025@gmail.com>
2016-11-17 19:26:51 -0800

    galleries and artists displaying on homepage

--- app/component/gallery/gallery-li/gallery-li.js
+++ app/component/gallery/gallery-li/gallery-li.js
@@ -4,7 +4,7 @@ require('./_gallery-li.scss');
 
 module.exports = {
   template: require('./gallery-li.html'),
-  controller: ['$log', 'galleryService',  GalleryLIController],
+  controller: ['$log', '$location', 'galleryService',  GalleryLIController],
   controllerAs: 'galleryLICtrl',
   bindings: {
     gallery: '<',
@@ -12,10 +12,17 @@ module.exports = {
   },
 };
 
-function GalleryLIController($log, galleryService){
+function GalleryLIController($log, $location, galleryService){
   $log.debug('init galleryLICtrl');
 
   this.showEditGallery = false;
+  this.homeView = false;
+
+  this.isHomeView = function(){
+    if ($location.url() === '/home'){
+      this.homeView = true;
+    }
+  };
 
   this.deleteGallery = function(){
     galleryService.deleteGallery(this.gallery._id, this.gallery.artistID)
