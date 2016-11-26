6736d90d18d75dca0190917f5f7cd88461d87340
Lee Broxson <broxsonl@gmail.com>
2016-11-26 13:08:56 -0800

    Removed the npm version definition in package.json. Also added back in if block.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
-require('dotenv').load({path: `${__dirname}/.env`});
+if (`${__dirname}/.env`) {
+  require('dotenv').load({path: `${__dirname}/.env`});
+}
 
 if (!process.env.API_URL || !process.env.NODE_ENV || !process.env.TITLE){
   console.error('ERROR: ng-template requires .env file');
