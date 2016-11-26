772d4a55824ba9c813f7716950fda1607bfea1a2
Lee Broxson <broxsonl@gmail.com>
2016-11-26 12:44:42 -0800

    Removed testing env path.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 'use strict';
 
-require('dotenv').load({path: `${__dirname}/.env`});
 if (process.env.NODE_ENV === 'testing') {
-  require('./test/lib/test-env');
+  require('dotenv').load({path: `${__dirname}/.env`});
 }
 if (!process.env.API_URL || !process.env.NODE_ENV || !process.env.TITLE){
   console.error('ERROR: ng-template requires .env file');
