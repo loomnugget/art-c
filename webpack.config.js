fb50981f12f8baa1543b1ba6bacf0ab25aaa957f
Lee Broxson <broxsonl@gmail.com>
2016-11-26 12:54:40 -0800

    Moved the env file require statement in the webpack config into an if block.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'testing') {
+if (`${__dirname}/.env`) {
   require('dotenv').load({path: `${__dirname}/.env`});
 }
+
 if (!process.env.API_URL || !process.env.NODE_ENV || !process.env.TITLE){
   console.error('ERROR: ng-template requires .env file');
   process.exit(1);
