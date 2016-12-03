fbf6b0f0a806462f35cd88436483b9afbefd8343
Claudia <claudia.cedfeldt@gmail.com>
2016-12-03 11:47:48 -0800

    added brighter carousel images with gradient-colors are just random and can be changed later

--- app/component/landing/carousel/carousel.js
+++ app/component/landing/carousel/carousel.js
@@ -14,19 +14,19 @@ function CarouselController($log){
   this.interval = 5000;
   this.slides = [
     {
-      image: 'http://i68.tinypic.com/2wbt6if.jpg',
+      image: 'http://i68.tinypic.com/dy0zd0.jpg',
       id: 0,
       title: 'Welcome',
       text: 'art-c is the home of art',
     },
     {
-      image: 'http://i67.tinypic.com/20qxv9l.jpg',
+      image: 'http://i68.tinypic.com/zludd3.jpg',
       id: 1,
       title: 'Artists',
       text: 'View artists and their galleries',
     },
     {
-      image: 'http://i66.tinypic.com/oqetlk.jpg',
+      image: 'http://i66.tinypic.com/nywdab.jpg',
       id: 2,
       title: 'Portfolio',
       text: 'Create a profile and galleries',
