ee205a01defd60e8ed0f2cffeba031c380b24c7d
Lee Broxson <broxsonl@gmail.com>
2016-12-03 11:21:22 -0800

    Fixed some test errors.

--- route/auth-router.js
+++ route/auth-router.js
@@ -64,7 +64,7 @@ authRouter.delete('/api/user/deleteAccount', bearerAuth, function(req, res, next
     let s3DeletePhotoArray = [];
     for(var i=0; i<photos.length; i++){
       s3DeletePhotoArray.push(s3.deleteObject({
-        Bucket: 'artc-staging-assets',
+        Bucket: `${process.env.AWS_BUCKET}`,
         Key: photos[i].objectKey,
       }).promise());
     }
