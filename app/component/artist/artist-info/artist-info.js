5a35edf6583409da198a335747c42829740f2dc9
meeksfred <maxfred0025@gmail.com>
2016-12-03 11:34:25 -0800

    artist profile delete functionality, deletes all associated galleries and listings

--- app/component/artist/artist-info/artist-info.js
+++ app/component/artist/artist-info/artist-info.js
@@ -5,18 +5,25 @@ require('./_artist-info.scss');
 module.exports = {
   template: require('./artist-info.html'),
   controllerAs: 'artistInfoCtrl',
-  controller: ['$log', ArtistInfoController],
+  controller: ['$log', '$location', 'artistService', ArtistInfoController],
   bindings: {
     artist: '<',
   },
 };
 
-function ArtistInfoController($log){
+function ArtistInfoController($log, $location, artistService){
   $log.debug('init artistInfoCtrl');
 
   this.showEditArtist = false;
 
   this.handleEditArtist = function(){
     this.showEditArtist = false;
+  };
+
+  this.deleteArtist = function(){
+    artistService.deleteArtist(this.artist)
+    .then(() => {
+      $location.url('/home');
+    });
   };
 }
